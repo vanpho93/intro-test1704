d7f28d95c51cf47527c9b35cdd601edb23dc390d
Van Pho Nguyen <vanpho02@gmail.com>
2018-05-15 21:06:37 +0700

    test db

--- test/user/signUp.test.js
+++ test/user/signUp.test.js
@@ -1,6 +1,7 @@
 const request = require('supertest');
 const { equal } = require('assert');
 const { app } = require('../../src/app');
+const { User } = require('../../src/models/user.model');
 
 describe('POST /user/signup', () => {
     it('Can sign up', async () => {
@@ -12,6 +13,16 @@ describe('POST /user/signup', () => {
         const response = await request(app)
         .post('/user/signup')
         .send(body);
-        console.log(response.body);
+        const { success, user } = response.body;
+        const { email, name, password, _id } = user;
+        equal(response.status, 200);
+        equal(success, true);
+        equal(email, 'teo1@gmail.com');
+        equal(password, '123');
+        equal(name, 'Teo Nguyen');
+        const userInDb = await User.findById(_id);
+        equal(userInDb.email, 'teo1@gmail.com');
+        equal(userInDb.password, '123');
+        equal(userInDb.name, 'Teo Nguyen');
     });
 });
