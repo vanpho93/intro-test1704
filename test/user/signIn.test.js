b417a2cad4c2ba19233e526307d860b03be07e3f
Van Pho Nguyen <vanpho02@gmail.com>
2018-05-17 19:38:59 +0700

    sign in test

--- test/user/signIn.test.js
+++ test/user/signIn.test.js
@@ -13,15 +13,26 @@ describe('POST /user/signin', () => {
         await request(app).post('/user/signup').send(body);
     });
 
-    it.only('Can sign in', async () => {
+    it('Can sign in', async () => {
+        const body = {
+            email: 'teo1@gmail.com',
+            password: '123'
+        };
+        const response = await request(app).post('/user/signin').send(body);
+        const { success, user } = response.body;
+        equal(success, true);
+        equal(user.name, 'Teo Nguyen');
+    });
+
+    it('Cannot sign in with invalid password', async () => {
         const body = {
             email: 'teo1@gmail.com',
             password: '1234'
         };
         const response = await request(app).post('/user/signin').send(body);
-        console.log(response.body);
-    });
-
-    it('Cannot sign in with invalid password', async () => {
+        const { success, user, message } = response.body;
+        equal(success, false);
+        equal(user, undefined);
+        equal(message, 'INVALID_USER_INFO');
     });
 });
