c2d366d3a9d4432be2920f7d38ad7457634d728f
Van Pho Nguyen <vanpho02@gmail.com>
2018-05-17 20:37:51 +0700

    user.service.js

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,13 +1,9 @@
 const { User } = require('../models/user.model');
 const { hash, compare } = require('bcrypt');
-const { sign } = require('../helpers/jwt');
+const { sign, verify } = require('../helpers/jwt');
 
 class UserService {
-    static async signIn(email, password) {
-        const user = await User.findOne({ email });
-        if (!user) throw new Error('INVALID_USER_INFO');
-        const same = await compare(password, user.password);
-        if (!same) throw new Error('INVALID_USER_INFO');
+    static async getUserObject(user) {
         const userInfo = user.toObject();
         const token = await sign({ _id: user._id });
         userInfo.token = token;
@@ -15,13 +11,26 @@ class UserService {
         return userInfo;
     }
 
+    static async signIn(email, password) {
+        const user = await User.findOne({ email });
+        if (!user) throw new Error('INVALID_USER_INFO');
+        const same = await compare(password, user.password);
+        if (!same) throw new Error('INVALID_USER_INFO');
+        return UserService.getUserObject(user);
+    }
+
     static async signUp(email, password, name) {
         const encrypted = await hash(password, 8);
         const user = new User({ email, password: encrypted, name });
         await user.save();
-        const userInfo = user.toObject();
-        delete userInfo.password;
-        return userInfo;
+        return UserService.getUserObject(user);
+    }
+
+    static async check(token) {
+        const { _id } = await verify(token);
+        const user = await User.findById(_id);
+        if (!user) throw new Error('CANNOT_FIND_USER');
+        return getUserObject(user);
     }
 }
 
