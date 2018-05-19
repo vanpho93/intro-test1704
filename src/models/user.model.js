42d04df7439b080d7a68a2c70c547e4a73d8a653
Van Pho Nguyen <vanpho02@gmail.com>
2018-05-19 19:50:34 +0700

    setup schemas

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    name: { type: String, required: true }
+    name: { type: String, required: true },
+    stories: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Story' }]
 });
 
 const User = mongoose.model('User', userSchema);
